3d5eaeffb07f597219ef24ba19b6f93a17d7a792
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-12-28 12:15:20 -0800

    adding design

--- venv/pacman.py
+++ venv/pacman.py
@@ -1,5 +1,6 @@
 from turtle import width
 import pygame
+from board import boards
 
 pygame.init()
 
@@ -9,11 +10,21 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 18)
+level = boards
+
+def draw_baord(lvl):
+    num1 = ((HEIGHT - 50) // 32)
+    num2 = (WIDTH // 30)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 4)
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill('black')
+    draw_board(level)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
