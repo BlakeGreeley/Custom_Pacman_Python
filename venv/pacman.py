31c8c9cfbbac23a5278c7a467ccf98bb48bac5e0
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-12-28 16:46:18 -0800

    updating design

--- venv/pacman.py
+++ venv/pacman.py
@@ -20,6 +20,9 @@ def draw_board(lvl):
     num2 = (WIDTH // 30)
     for i in range(len(level)):
         for j in range(len(level[i])):
+            
+            # each design application on the board from 1-9 spaces v
+
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 4)
             if level[i][j] == 2:
@@ -30,11 +33,14 @@ def draw_board(lvl):
             if level[i][j] == 4:
                 pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + (0.5*num1)), 3)
-
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], 0, PI/2, 3)
             
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + (0.5*num1)), 3)
+            
+            # each design application on the board from 1-9 spaces ^
 run = True
 while run:
     timer.tick(fps)
