ab0380550195cfb6fa24868147fec832dfda50f5
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-12-29 11:51:08 -0800

    update 12/29/22

--- venv/pacman.py
+++ venv/pacman.py
@@ -21,7 +21,7 @@ def draw_board(lvl):
     for i in range(len(level)):
         for j in range(len(level[i])):
             
-            # each design application on the board from 1-9 spaces v
+            # each design application on the board from 1-9 spaces (below)
 
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 4)
@@ -34,16 +34,16 @@ def draw_board(lvl):
                 pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + (0.5*num1)), 3)
             if level[i][j] == 5:
-                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], 0, PI/2, 3)
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4)) -2, (i * num1 + (0.5*num1)), num2, num1], 0, PI/2, 3)
 
             if level[i][j] == 6:
-                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], 0, 2/PI, 3)
+                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], PI/2, PI , 3)
             
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + (0.5*num1)), 3)
             
-            # each design application on the board from 1-9 spaces ^
+            # each design application on the board from 1-9 spaces ^(above)
 run = True
 while run:
     timer.tick(fps)
