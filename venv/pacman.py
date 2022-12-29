b89a8cdfd62de4785e7a3e8997ba5c9488a17e64
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-12-29 12:00:47 -0800

    update board creation

--- venv/pacman.py
+++ venv/pacman.py
@@ -39,6 +39,12 @@ def draw_board(lvl):
             if level[i][j] == 6:
                 pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], PI/2, PI , 3)
             
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i * num1 - (0.5*num1)), num2, num1], PI, 3*PI/2, 3)
+
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4)) - 2, (i * num1 - (0.5*num1)), num2, num1], 3*PI/2, 2*PI , 3)
+
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + (0.5*num1)), 3)
