7155846c769ceac3cb8728c9e9a8a8faf224fbb7
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2023-01-04 16:54:38 -0800

    update 1/4/23

--- venv/pacman.py
+++ venv/pacman.py
@@ -77,28 +77,48 @@ def draw_player():
 # Drawing pacman above  ^
 
 # This function is for checking collisions and allowed pacman to go forward or stopped
-def check_position(center_x, center_y):
+def check_position(centerx, centery):
     turns = [False, False, False, False]
     num1 = (HEIGHT - 50)//32
     num2 = (WIDTH/30)
     num3 = 15
-    if center_x // 30 < 29:
+    if centerx // 30 < 29:
         if direction == 0:
-            if level[center_y//num1][(center_x - num3) // num2] < 3:
+            if level[centery // num1][(centerx - num3) // num2] < 3:
                 turns[1] = True
         if direction == 1:
-            if level[center_y//num1][(center_x + num3) // num2] < 3:
+            if level[centery // num1][(centerx + num3) // num2] < 3:
                 turns[0] = True
         if direction == 2:
-            if level[(center_y+num3)//num1][center_x // num2] < 3:
+            if level[(centery + num3)//num1][centerx // num2] < 3:
                 turns[3] = True
         if direction == 3:
-            if level[(center_y - num3)//num1][center_x // num2] < 3:
+            if level[(centery - num3)//num1][centerx // num2] < 3:
                 turns[2] = True
+        
         if direction == 2 or direction == 3:
             if 12 <= center_x % num2 <= 18:
-                if level[(center_y+num3)//num1][center_x // num2] < 3:
+                if level[(centery + num3)//num1][centerx // num2] < 3:
                     turns[3] = True
+                if level[(centery - num3)//num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= center_y % num1 <= 18:
+                if level[centery // num1][centerx - num2 // num2] < 3:
+                    turns[3] = True
+                if level[center_y // num1][centerx + num2 // num2] < 3:
+                    turns[2] = True
+        
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num1) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num1) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level[centery // num1][centerx - num3 // num2] < 3:
+                    turns[1] = True
+                if level[centery // num1][centerx + num3 // num2] < 3:
+                    turns[0] = True
 
     else:
         turns[0] = True
@@ -121,9 +141,9 @@ while run:
     screen.fill('black')
     draw_board(level)
     draw_player()
-    center_x = player_x + 23
-    center_y = player_y + 24
-    turns_allowed = check_position(center_x, center_y)
+    centerx = player_x + 23
+    centery = player_y + 24
+    turns_allowed = check_position(centerx, centery)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
